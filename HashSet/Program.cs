fdc34f6ad719fec769d5f95cbe80dbfce7ab554b
0baydullah <obaydullahkhaan@gmail.com>
2024-12-05 18:02:24 +0600

    Hashtag counter added

--- HashSet/Program.cs
+++ HashSet/Program.cs
@@ -1,4 +1,6 @@
-﻿Home:
+﻿using System.ComponentModel.DataAnnotations;
+
+Home:
 
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Green;
@@ -10,7 +12,7 @@ Console.BackgroundColor = ConsoleColor.Black;
 
 Console.WriteLine("Enter command to execute function...!! Enter 0 to RESET & -1 to EXIT : ");
 Console.WriteLine("    1 - Available Letters");
-//Console.WriteLine("    2 - Word Counter");
+Console.WriteLine("    2 - HashTag Calculator");
 //Console.WriteLine("    3 - Most Frequent Character(s)");
 
 int input = Convert.ToInt32(Console.ReadLine());
@@ -24,15 +26,47 @@ switch (input)
     case 1:
         AvailableLetters();
         goto case 0;
-    //case 2:
-    //    WordCounter();
-    //    goto case 0;
+    case 2:
+        HashTagCalculator();
+        goto case 0;
     //case 3:
     //    MostFrequentCharacters();
     //    goto case 0;
     default:
         Console.WriteLine("Not a VALID Command...Try Again!");
         goto case 0;
+}
+
+void HashTagCalculator()
+{
+    Console.WriteLine("Enter a Paragraphs with multiple Hashtags : ");
+    String text = Console.ReadLine(); 
+
+    String[] splittedText = text.Split(' ');
+
+    //var hashTags = new SortedSet<string>(splittedText.Where(x => x.StartsWith('#')));
+    var hashTags = new SortedSet<string>();
+
+    foreach(var x in splittedText) hashTags.Add(x.Trim());
+
+    foreach(var tag in hashTags)
+    {
+        int len = tag.Length;
+        if (char.IsLetter(tag[len - 1]))
+        {
+            var tagg = tag;
+            hashTags.Add(tag.Remove(len - 1));
+        }
+        else
+            hashTags.Add(tag);
+
+    }
+    Console.WriteLine("HashTags are : ");
+    foreach(var tag in hashTags)
+    {
+        Console.WriteLine(tag);
+    }
+
 }
 
 void AvailableLetters()
@@ -45,10 +79,9 @@ void AvailableLetters()
 
     foreach (var c in text)
     {
-        if(char.IsLetter(c))
-        set.Add(c);
+        if (char.IsLetter(c))
+            set.Add(c);
     }
-
 
     string output = string.Join(", ", set);
     Console.WriteLine($"Available Characters are : { output.ToString()}" );
