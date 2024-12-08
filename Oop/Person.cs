7101601dfdbe4bae1f45b471eb89efa9f2c92be7
0baydullah <obaydullahkhaan@gmail.com>
2024-12-08 16:41:09 +0600

    First class and method ToString()

---
+++ Oop/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop
+{
+    internal class Person
+    {
+        
+        public string Name { get; set; }
+        //public Person(string name)
+        //{
+        //    this.Name = name;
+        //}
+        public override string ToString()
+        {
+            return "Hello ! My name is " + Name;
+        }
+
+        //~Person()
+        //{
+        //    Name = string.Empty;
+        //}
+    }
+}
---
+++ Oop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using Oop;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int totalPerson = 3;
+        Person[] persons = new Person[totalPerson];
+
+        for(int i = 0; i < totalPerson; i++)
+        {
+            Console.Write("Enter a persons Name : ");
+
+            persons[i] = new Person()
+            {
+                Name = Console.ReadLine()
+            };
+        }
+
+        foreach(Person person in persons)
+        {
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
