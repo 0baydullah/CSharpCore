c2518ed8908a99cc5b25358a53c542f0b8ee74b7
0baydullah <obaydullahkhaan@gmail.com>
2024-12-08 17:00:17 +0600

    Constructors and destructors

---
+++ ConstructorsAndDestructors/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopConstructorsAndDestructors
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "hi !! im " + Name;
+        }
+
+        ~Person()
+        {
+            this.Name = string.Empty;
+        }
+    }
+}
---
+++ ConstructorsAndDestructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using OopConstructorsAndDestructors;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int total = 3;
+
+        Person[] persons = new Person[total];
+
+        for(int i = 0;  i < total; i++)
+        {
+            Console.WriteLine("Enter a name  :  ");
+            persons[i] = new Person(Console.ReadLine()); 
+        }
+
+        foreach(Person person in persons)
+        {
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
