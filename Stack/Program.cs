d80f740451ce8b31ff184846271d91e7657bce2c
0baydullah <obaydullahkhaan@gmail.com>
2024-12-08 10:39:59 +0600

    Bracket Sequence checker added

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,4 +1,6 @@
-﻿Home:
+﻿using System.Runtime.InteropServices;
+
+Home:
 
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Green;
@@ -10,7 +12,7 @@ Console.BackgroundColor = ConsoleColor.Black;
 
 Console.WriteLine("Enter command to execute function...!! Enter 0 to RESET & -1 to EXIT : ");
 Console.WriteLine("    1 - Reverse a String");
-Console.WriteLine("    2 - ");
+Console.WriteLine("    2 - Check bracket sequence");
 //Console.WriteLine("    3 - Most Frequent Character(s)");
 
 int input = Convert.ToInt32(Console.ReadLine());
@@ -24,9 +26,9 @@ switch (input)
     case 1:
         StringReverser();
         goto case 0;
-    /*case 2:
-        HashTagCalculator();
-        goto case 0;*/
+    case 2:
+        BracketSequenceValidator();
+        goto case 0;
     //case 3:
     //    MostFrequentCharacters();
     //    goto case 0;
@@ -35,9 +37,51 @@ switch (input)
         goto case 0;
 }
 
+void BracketSequenceValidator()
+{
+    bool output = true;
+
+    Console.Write("Enter a bracket sequence to check its valid or not : ");
+
+    String data = Console.ReadLine();
+    Stack<char> brackets = new Stack<char>();
+    foreach(var c in data)
+    {
+        if(c == '[' || c== '{' || c == '(')
+        {
+            brackets.Push(c);
+        }
+        else
+        {
+            if(brackets.Count() == 0)
+            {
+                output = false;
+                break;
+            }
+            else
+            {
+                var ch = brackets.Pop();
+                if(ch == '(' && c == ')' || ch == '{' && c == '}' || ch == '[' && c == ']')
+                {
+                    continue;
+                }
+                else
+                {
+                    output = false;
+                }
+            }
+        }
+    }
+    if (brackets.Count() != 0) output = false;
+
+    if (output == true) Console.WriteLine("VALID BRACKETS"); else Console.WriteLine("INVALID BRACKETS");
+
+    Console.WriteLine("\n+++ END +++\n");
+}
+
 void StringReverser()
 {
-    Console.WriteLine("Enter a String to Reverse : ");
+    Console.Write("Enter a String to Reverse : ");
     string data = Console.ReadLine();
 
     /**
