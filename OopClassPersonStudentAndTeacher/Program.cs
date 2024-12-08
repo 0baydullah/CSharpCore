51c60e2466f38933993eb89b979276385ab2362f
0baydullah <obaydullahkhaan@gmail.com>
2024-12-08 17:20:16 +0600

    Class person, student and teacher , inheritance

---
+++ OopClassPersonStudentAndTeacher/Program.cs
@@ -0,0 +1,18 @@
+﻿using OopClassPersonStudentAndTeacher;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Greet();
+
+        Student student = new Student() { Age = 26 };
+        student.Greet();
+        student.ShowAge();
+
+        Teacher teacher = new Teacher() { Age = 39};
+        teacher.Greet();
+        teacher.Explain();
+    }
+}
---
+++ OopClassPersonStudentAndTeacher/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopClassPersonStudentAndTeacher
+{
+    internal class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm Studing...");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"My age is : {Age} years old");
+        }
+    }
+}
